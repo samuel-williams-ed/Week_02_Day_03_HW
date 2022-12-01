345eed2e3899faa4d0e29e3e9de419526a2bc986
Samuel Williams <samuel.williams.ed@gmail.com>
2022-12-01 09:49:28 +0000

    Built food.py

---
+++ src/food.py
@@ -0,0 +1,8 @@
+class Food():
+    def __init__(self, input_name, input_price, input_rejuvenation):
+        self.name = input_name
+        self.price = input_price
+        self.rejuvenation_level = input_rejuvenation
+
+    #Methods
+    
